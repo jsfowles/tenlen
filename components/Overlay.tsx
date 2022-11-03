cc67bd417a58c209fbf528746779754a87366907
Jacob Fowles <js.fowles@gmail.com>
2022-11-03 11:22:57 -0600

    refactor a few things, including the overlay

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Overlay = () => (
-  <div className="dotted z-10 w-full h-full absolute bottom-6 left-6 rounded-2xl w-[83%] h-[88%]" />
+  <div className="dotted-pattern z-10 w-full h-full absolute bottom-6 left-6 rounded-2xl w-[83%] h-[88%]" />
 );
 
 export default Overlay;
